efdecffc402c46f6ac935541d9a5b528349b80ea
“jackal7819” <jackal3166@gmail.com>
2023-01-29 20:56:47 +0200

    create SelectsCard

--- src/components/Selects.jsx
+++ src/components/Selects.jsx
@@ -6,46 +6,17 @@ import Maldives from '../assets/maldives.jpg';
 import Maldives2 from '../assets/maldives2.jpg';
 import Maldives3 from '../assets/maldives3.jpg';
 import KeyWest from '../assets/keywest.jpg';
+import SelectsCard from './SelectsCard';
 
 const Selects = () => {
     return (
         <div className='max-w-[1240px] mx-auto px-4 py-16 grid sm:grid-cols-2 lg:grid-cols-3 gap-4'>
-            <div className='relative'>
-                <img src={BoraBora} alt="Maldives" />
-                <div className='bg-gray-900/30 absolute top-0 w-full h-full'>
-                    <p className='left-4 bottom-4 text-2xl font-bold text-white absolute'>Bora Bora</p>
-                </div>
-            </div>
-            <div className='relative'>
-                <img src={BoraBora2} alt="Maldives" />
-                <div className='bg-gray-900/30 absolute top-0 w-full h-full'>
-                    <p className='left-4 bottom-4 text-2xl font-bold text-white absolute'>Bora Bora</p>
-                </div>
-            </div>
-            <div className='relative'>
-                <img src={Maldives} alt="Maldives" />
-                <div className='bg-gray-900/30 absolute top-0 w-full h-full'>
-                    <p className='left-4 bottom-4 text-2xl font-bold text-white absolute'>Maldives</p>
-                </div>
-            </div>
-            <div className='relative'>
-                <img src={Maldives2} alt="Maldives" />
-                <div className='bg-gray-900/30 absolute top-0 w-full h-full'>
-                    <p className='left-4 bottom-4 text-2xl font-bold text-white absolute'>Maldives</p>
-                </div>
-            </div>
-            <div className='relative'>
-                <img src={Maldives3} alt="Maldives" />
-                <div className='bg-gray-900/30 absolute top-0 w-full h-full'>
-                    <p className='left-4 bottom-4 text-2xl font-bold text-white absolute'>Maldives</p>
-                </div>
-            </div>
-            <div className='relative'>
-                <img src={KeyWest} alt="Maldives" />
-                <div className='bg-gray-900/30 absolute top-0 w-full h-full'>
-                    <p className='left-4 bottom-4 text-2xl font-bold text-white absolute'>Key West</p>
-                </div>
-            </div>
+            <SelectsCard bg={BoraBora} text='Bora Bora'/>
+            <SelectsCard bg={BoraBora2} text='Antigua'/>
+            <SelectsCard bg={Maldives} text='Maldives'/>
+            <SelectsCard bg={Maldives2} text='Cozumel'/>
+            <SelectsCard bg={Maldives3} text='Jamaica'/>
+            <SelectsCard bg={KeyWest} text='Key West'/>
         </div>
     )
 };
